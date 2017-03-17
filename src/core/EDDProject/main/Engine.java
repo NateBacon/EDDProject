ae342d06eb69d8e79c7a4646b9b31920988ec1d8
NateBacon <bakernathan1299@gmail.com>
2017-03-17 15:40:06 -0400

    it work
    got im

---
+++ src/core/EDDProject/main/Engine.java
@@ -0,0 +1,9 @@
+package core.EDDProject.main;
+
+public class Engine {
+	private static Engine single = new Engine();//there can only be one
+	
+	public static Engine getEngine(){
+		return single;
+	}
+}
