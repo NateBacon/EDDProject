cedef7e4b203919b88e20c4d193de6ec76e96661
AdamFrank37 <frankad.37@gmail.com>
2017-04-24 12:12:15 -0400

    changed it so that the Engine is in a JPanel
    skrt

--- src/core/EDDProject/main/Main.java
+++ src/core/EDDProject/main/Main.java
@@ -18,11 +18,13 @@ public class Main {
 		Window frame2 = new Window(640,640,"UI Window");
 		frame2.setBackground(Color.BLACK);
 		JPanel daPanel = new JPanel();
+		
 		frame2.setLayout(new BorderLayout());
 		frame2.add(daPanel,BorderLayout.CENTER);
 		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
-		Engine engine = new Engine(canvas, frame);
+		Engine engine = new Engine(canvas, daPanel);
 		engine.addShape(0.3f, 0.4f, 0.3f, new Color3f(.35f, .2f, .001f));
+		
 	}
 
 }
